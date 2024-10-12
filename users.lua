453cde97a1700eb6ea3b8da6284688b1cbb8fe5e
Z.. <137589536+Owner1213@users.noreply.github.com>
2024-10-12 16:25:57 -0400

    Update users.lua

--- users.lua
+++ users.lua
@@ -1,7 +1,27 @@
 return {
-        "NotZWare_VC",
-        "typer_progamerspeed",
-        "dawyftgjhklddawda",
-        "SalamanderMammalton",
-        "Justheoofy"
+    [1] = {
+        u = "NotZWare_VC",
+        t = "Developer",
+        tc = Color3.fromRGB(255, 0, 0)
+    },
+    [2] = {
+        u = "typer_progamerspeed",
+        t = "Developer",
+        tc = Color3.fromRGB(255, 0, 0)
     }
+    [3] = {
+        u = "dawyftgjhklddawda",
+        t = "Developer",
+        tc = Color3.fromRGB(255, 0, 0)
+    }
+    [4] = {
+        u = "SalamanderMammalton",
+        t = "Owner",
+        tc = Color3.fromRGB(255, 0, 0)
+    }
+    [5] = {
+        u = "Justheoofy",
+        t = "Owner",
+        tc = Color3.fromRGB(255, 0, 0)
+    }
+}
