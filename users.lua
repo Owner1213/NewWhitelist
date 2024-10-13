a7f53d1d0af896ccd3d74b7b23efbf7fba1589c2
Z.. <137589536+Owner1213@users.noreply.github.com>
2024-10-12 21:12:03 -0400

    Update users.lua

--- users.lua
+++ users.lua
@@ -2,26 +2,31 @@ return {
     [1] = {
         u = "NotZWare_VC",
         t = "Developer",
-        tc = Color3.fromRGB(255, 0, 0)
+        tc = Color3.fromRGB(255, 0, 0),
+        hwid = "25eeb711-5346-4b92-bce3-1a2b85119b56"
     },
     [2] = {
         u = "typer_progamerspeed",
         t = "Developer",
-        tc = Color3.fromRGB(255, 0, 0)
+        tc = Color3.fromRGB(255, 0, 0),
+        hwid = "25eeb711-5346-4b92-bce3-1a2b85119b56"
     }
     [3] = {
         u = "dawyftgjhklddawda",
         t = "Developer",
-        tc = Color3.fromRGB(255, 0, 0)
+        tc = Color3.fromRGB(255, 0, 0),
+        hwid = "25eeb711-5346-4b92-bce3-1a2b85119b56"
     }
     [4] = {
         u = "SalamanderMammalton",
         t = "Owner",
-        tc = Color3.fromRGB(255, 0, 0)
+        tc = Color3.fromRGB(255, 0, 0),
+        hwid = "d4f55672-4038-45da-abe5-15f289f61e91"
     }
     [5] = {
         u = "Justheoofy",
         t = "Owner",
-        tc = Color3.fromRGB(255, 0, 0)
+        tc = Color3.fromRGB(255, 0, 0),
+        hwid = "d4f55672-4038-45da-abe5-15f289f61e91"
     }
 }
