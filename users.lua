2295ac65aa001e7cbab4591e03bec3b4f4a58d5b
Z.. <137589536+Owner1213@users.noreply.github.com>
2024-10-12 21:37:21 -0400

    Update users.lua

--- users.lua
+++ users.lua
@@ -2,30 +2,35 @@ return {
     [1] = {
         u = "NotZWare_VC",
         t = "Developer",
+        dcn = "Snow",
         tc = Color3.fromRGB(255, 0, 0),
         hwid = "25eeb711-5346-4b92-bce3-1a2b85119b56"
     },
     [2] = {
         u = "typer_progamerspeed",
         t = "Developer",
+        dcn = "Snow",
         tc = Color3.fromRGB(255, 0, 0),
         hwid = "25eeb711-5346-4b92-bce3-1a2b85119b56"
-    }
+    },
     [3] = {
         u = "dawyftgjhklddawda",
         t = "Developer",
+        dcn = "Snow",
         tc = Color3.fromRGB(255, 0, 0),
         hwid = "25eeb711-5346-4b92-bce3-1a2b85119b56"
-    }
+    },
     [4] = {
         u = "SalamanderMammalton",
         t = "Owner",
+        dcn = "Preston",
         tc = Color3.fromRGB(255, 0, 0),
         hwid = "d4f55672-4038-45da-abe5-15f289f61e91"
-    }
+    },
     [5] = {
         u = "Justheoofy",
         t = "Owner",
+        dcn = "Preston",
         tc = Color3.fromRGB(255, 0, 0),
         hwid = "d4f55672-4038-45da-abe5-15f289f61e91"
     }
